a4aa92fdabcb69f02d716207b1f462c0e996aedb
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-08 06:38:53 -0500

    Testing passes for workorder_spec.

--- spec/models/workorder_spec.rb
+++ spec/models/workorder_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Workorder, type: :model do 
     describe 'relationships' do 
-
+        it { should have_many :customers }
+        it { should have_many :admins }
     end
 
     describe 'validations' do 
-        
+        it { should validate_presence_of :description }
+        it { should validate_presence_of :work_type }
     end
 end
