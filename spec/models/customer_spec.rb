4e4d551bd9647721cd2c3d6cd6107d1ac9755e67
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-08 06:40:33 -0500

    Testing passes for customer spec.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do 
     describe 'relationships' do 
-
+        it { should belong_to :workorder }
     end
 
     describe 'validations' do 
-        
+        it { should validate_presence_of :name }
+        it { should validate_presence_of :phone_number }
+        it { should validate_presence_of :address }
+        it { should validate_presence_of :email }
     end
 end
