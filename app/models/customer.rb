92ea24914fd1f96ee0233faae2c9935ef396ec61
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-12 06:28:10 -0500

    Model adjustment for email values. Testing fails right now- moving to debugging.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,8 @@ class Customer < ApplicationRecord
     belongs_to :workorder
     # has_many :admins, through: :workorder
 
-    validates_presence_of :name, :phone_number, 
-                        :address, :email, :workorder_id
+    validates :name, :phone_number, :address, 
+    :email, :workorder_id, presence: true
+    validates :email, uniqueness: true
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -8,6 +8,9 @@ RSpec.describe Admin, type: :model do
 
     describe 'validations' do 
         it { should validate_presence_of :email }
+        it { should validate_uniqueness_of(:email) }
+        it { should allow_value('user@example.com').for(:email) }
+        it { should_not allow_value('invalid_email').for(:email) }    
         it { should validate_presence_of :workorder_id }
     end
 end
