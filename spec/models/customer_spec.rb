cae7cea9a2b1978471ed382a5986c4ffe3c50833
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-13 06:12:26 -0500

    Additional testing added to customer_spec for email verification. Testing passes. Yeet.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe Customer, type: :model do
     end
 
     describe 'validations' do 
+        subject { described_class.create!(name: 'Ernie', email: 'admin@example.com', address: '123 Mulberry Lane', phone_number: '867-5309', workorder: Workorder.create!(description: "Test", work_type: "Repair")) }
+
         it { should validate_presence_of :name }
         it { should validate_presence_of :phone_number }
         it { should validate_presence_of :address }
         it { should validate_presence_of :email }
+        it { should validate_uniqueness_of(:email) }
+        it { should allow_value('user@example.com').for(:email) }
+        it { should_not allow_value('invalid_email').for(:email) }    
         it { should validate_presence_of :workorder_id }
     end
 end
