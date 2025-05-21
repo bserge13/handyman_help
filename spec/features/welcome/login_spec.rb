58ad7cfd09ba78abdbe1de371c656408d79ad30c
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-21 06:39:32 -0500

    Testing expectations written for landing page. Moving to TDD.

--- spec/features/welcome/login_spec.rb
+++ spec/features/welcome/login_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Landing Page', type: :feature do
     describe 'welcome' do 
         it 'has a landing page' do 
-            
+            visit root_path
+
+            expect(page).to have_content('Welcome to Handyman Helper!')
         end
     end
 end 
