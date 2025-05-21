25572ec1c6441112b731cdde96f64002766e9ea8
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-21 06:38:28 -0500

    Landing page testing.

--- spec/features/welcome/login_spec.rb
+++ spec/features/welcome/login_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe 'Landing Page', type: :feature do
     describe 'welcome' do 
-        
+        it 'has a landing page' do 
+            
+        end
     end
 end 
