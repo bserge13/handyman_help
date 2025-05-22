dceb0a34109e242a36805dcb2fc6772e1bd7d657
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-22 06:25:05 -0500

    Login spec file initialized, renamed original to landing page spec.

---
+++ spec/features/welcome/landing_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Landing Page', type: :feature do
+    describe 'welcome' do 
+        it 'has a landing page' do 
+            visit root_path
+
+            expect(page).to have_content('Welcome to Handyman Helper!')
+        end
+
+        it 'has a login field' do 
+            visit root_path
+
+            expect(page).to have_field
+        end
+    end
+end 
