dae3639a0cd461a67502104e661c3babe29078a3
Blake Sergesketter <blakesergesketter@gmail.com>
2025-05-23 06:33:33 -0500

    TEsting almost passing. Creating a 'create profile' button.

--- spec/features/welcome/landing_page_spec.rb
+++ spec/features/welcome/landing_page_spec.rb
@@ -10,12 +10,11 @@ RSpec.describe 'Landing Page', type: :feature do
 
         it 'has options to create a profile or log in' do 
             visit root_path
-
+save_and_open_page
             expect(page).to have_field :email
             expect(page).to have_field :password 
             expect(page).to have_button('login')
 
-            expect(page).to have_button('Login')
             expect(page).to have_button('Create Profile')
         end
     end
